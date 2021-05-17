18bba35977593f26af9c87a5eb6c9d5a37f799ab
Rohan Ganguly <rohganguly@gmail.com>
2021-05-16 19:59:24 -0500

    finishing authorization for reviews

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,32 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const { campgroundSchema } = require('../schemas.js');
-const { isLoggedIn } = require('../middleware');
+// const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
-const ExpressError = require('../utils/ExpressError');
+// const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-
-const validateCampground = (req, res, next) => {
-	const { error } = campgroundSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map((el) => el.message).join(',');
-		throw new ExpressError(msg, 400);
-	} else {
-		next();
-	}
-};
-const isAuthor = async (req, res, next) => {
-	const { id } = req.params;
-	const campground = await Campground.findById(id);
-
-	if (!campground.author.equals(req.user._id)) {
-		req.flash('error', 'You do not have permission to do that!');
-		return res.redirect(`/campgrounds/${id}`);
-	} else {
-		next();
-	}
-};
 
 router.get(
 	'/',
@@ -57,9 +36,14 @@ router.get(
 	'/:id',
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id)
-			.populate('reviews')
+			.populate({
+				path: 'reviews',
+				populate: {
+					path: 'author',
+				},
+			})
 			.populate('author');
-		console.log(campground.author);
+		console.log(campground);
 		if (!campground) {
 			req.flash('error', 'Cannot find that campground!');
 			return res.redirect('/campgrounds');
